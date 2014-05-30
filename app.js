21b03bcd98406d1ccf1f0c72438adcd82b7d10a2
John D. Storey <johndangerstorey@gmail.com>
2014-05-30 15:52:53 -0600

    Update Commit

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
 var app = angular.module('AngularFriends',[]);
+
+// $.get('https://s3.amazonaws.com/intuiplan_company_files/production/files/public/FriendData.json');
---
+++ friendService.js
@@ -0,0 +1,7 @@
+app.factory('friendService', function ($http){
+	return {
+		getFriends: function(){
+			return $http.get('https://s3.amazonaws.com/intuiplan_company_files/production/files/public/FriendData.json');
+		}
+	}
+});
